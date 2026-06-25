ffd62e60ef3df070adc7e1712801395d9172493c
Sora <definiteheavy@gmail.com>
2026-06-25 13:02:47 +0800

    lab 7 added timeout for env

--- lab7tasksubmission.py
+++ lab7tasksubmission.py
@@ -1,9 +1,13 @@
 import simpy
 
 def student(env):
-    print('Start studying at', env.now)
-    yield env.timeout(2)
-    print('Take a break at', env.now)
+    print('Before study timeout at', env.now)
+    yield env.timeout(5)
+    print('After study timeout at', env.now)
+
+    print('Before rest timeout at', env.now)
+    yield env.timeout(1)
+    print('After rest timeout at', env.now)
 
 env = simpy.Environment()
 env.process(student(env))

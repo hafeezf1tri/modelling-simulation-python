839fbbfdb1714acd8bacccf5434e380fc8634cda
Sora <definiteheavy@gmail.com>
2026-06-25 12:43:05 +0800

    lab7 add

--- lab7tasksubmission.py
+++ lab7tasksubmission.py
@@ -1,1 +1,10 @@
 import simpy
+
+def student(env):
+    print('Start studying at', env.now)
+    yield env.timeout(5)
+    print('Take a break at', env.now)
+
+env = simpy.Environment()
+env.process(student(env))
+env.run()
